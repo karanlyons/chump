a0e500dd2cd98bd95e3889c2da559d43a7877ce8
Karan Lyons <karan@karanlyons.com>
2014-05-16 02:05:44 -0400

    Bump version number.

--- chump/__init__.py
+++ chump/__init__.py
@@ -11,7 +11,7 @@ from email.utils import parsedate
 import requests
 
 
-VERSION = (1, 3, 1)
+VERSION = (1, 3, 2)
 
 __title__ = 'Chump'
 __version__ = '.'.join((str(i) for i in VERSION)) # str for compatibility with setup.py under Python 3.
